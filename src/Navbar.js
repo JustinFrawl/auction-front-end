e1f12da715569103e1303c285ec71f01b15cd14a
Vijay Raju <vijaypraju@gmail.com>
2018-05-15 20:54:42 -0700

    removed logs

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const Navbar = props => {
-  console.log("navbar props", props)
 
   const token = localStorage.getItem('token');
 
